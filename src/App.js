a29cbaa6b22b3d441917b1eed9b32aafa86e91d3
Ricquel215 <ricqueljh215@gmail.com>
2025-04-27 18:06:37 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export default function App() {
         <a
           href="https://github.com/Ricquel215/rjh-react-weather"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on GitHub
         </a>
