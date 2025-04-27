4ce49e17bd278e60ae9aa4aa94e58e2641977c76
Ricquel215 <ricqueljh215@gmail.com>
2025-04-27 18:36:28 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Weather from "./Weather";
+import Weather from "./Weather"; // Import the Weather component
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <Weather defaultCity="New York" /> {/* Make sure this line is included */}
       <footer>
         This project is coded by Ricquel Harper and is{" "}
         <a
